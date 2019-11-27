d6d462688585f97b39697363c750ea9a9ec92950
amosavi@ualberta.ca <alireza19@DESKTOP-0VEJ48T.localdomain>
2019-11-27 14:34:09 -0700

    speech recognition phase complete

---
+++ audio.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+
+
+def main():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+        print("please say something...")
+
+        audio = r.listen(source)
+
+        try:
+            phrase = r.recognize_google(audio)
+            print("you have said: \n" + phrase)
+
+        except Exception as e:
+            print("Error: " + str(e))
+
+
+if __name__ == "__main__":
+    main()
